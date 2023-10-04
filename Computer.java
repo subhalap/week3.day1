02a15eeb4cff8598599655a7ee7af2ff9a245e2a
subhalap <51281496+subhalap@users.noreply.github.com>
2023-10-04 21:59:05 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,15 @@
+package org.system;
+
+public class Computer {
+	
+	public void computerModel() {
+		
+		System.out.println("Computer mode");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
